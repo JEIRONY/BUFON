6809cea6c166a79705ae1373676d99198c0c88a1
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-07-16 20:58:38 -0500

    Update yta.js

--- plugins/yta.js
+++ plugins/yta.js
@@ -6,6 +6,6 @@ let server = (args[1] || servers[0]).toLowerCase()
 let { dl_link, thumb, title, filesize, filesizeF } = await yta(args[0], servers.includes(server) ? server : servers[0])
 conn.sendFile(m.chat, dl_link, title + '.mp3', null, m, false, { mimetype: 'audio/mp4' })}              
 handler.help = ['mp3', 'a'].map(v => 'yt' + v + ` <url> [server: ${servers.join(', ')}]`)
-handler.tags = ['general']
+handler.tags = ['general'] 
 handler.command = /^yt(a|mp3)$/i
 module.exports = handler
