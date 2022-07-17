109c5842199c7d6c3e3630e6850e2903c297a2ac
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-07-16 21:47:01 -0500

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -57,11 +57,7 @@ let menu =`
 ┗━━━━━━━━━━━━━┛
 `.trim()
 let mentionedJid = [who]
-conn.send3ButtonImg(m.chat, pp, menu, '©BUFON', '𝙼𝙴𝙽𝚄 𝚂𝙸𝙼𝙿𝙻𝙴', `#menusimple`, '𝙼𝙴𝙽𝚄 𝙰𝚄𝙳𝙸𝙾𝚂', `#menuaudios`, '𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂', `#grupos`, m, false, { contextInfo: { mentionedJid }})   
-//await await await await await await conn.sendFile(m.chat, vn, 'mariana.mp3', null, m, true, {
-//type: 'audioMessage', 
-//ptt: true 
-//})
+
 }
 handler.command = /^(menu|menú|memu|memú|help|info|comandos|allmenu|2help|menu1.2|ayuda|commands|commandos)$/i
 handler.fail = null
