415436c7a7eadd9ae5b09317147ad62af3de4710
JEIRONY <82543429+JEIRONY@users.noreply.github.com>
2022-07-16 20:57:54 -0500

    Update tts.js

--- plugins/tts.js
+++ plugins/tts.js
@@ -1,7 +1,7 @@
 let gtts = require('node-gtts')
 let fs = require('fs')
 let path = require('path')
-let { spawn } = require('child_process')
+let { spawn } = require('child_process') 
 let handler = async (m, { conn, args, text }) => {
 let lang = 'pt'
 if (args[0].length === 2) lang = args[0]
